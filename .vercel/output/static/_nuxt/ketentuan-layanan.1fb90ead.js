199500c446ee99d1e4edb86139c58b028d29eb89
themeprys <themeprys@gmail.com>
2024-07-01 09:11:19 +0700

    add onbparding

---
+++ .vercel/output/static/_nuxt/ketentuan-layanan.1fb90ead.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as t,a as o}from"./entry.b5ac3aa9.js";const a={};function n(r,c,s,p,_,f){return t(),o("div")}const d=e(a,[["render",n]]);export{d as default};
